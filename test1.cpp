6895c975ebf0e2c14f7de8a18ae05e57dc0afc45
nhatlee0620 <nhatlee0620@gmail.com>
2022-12-10 12:39:45 +0700

    them 1 dong nua o local

--- test1.cpp
+++ test1.cpp
@@ -5,6 +5,7 @@ int main() {
     cout << "Them 1 dong trong test1.cpp" << endl;
     cout << "Them 1 dong trong test1.cpp o tren repository" << endl;
     cout << "Them 1 dong vao test1.cpp local" << endl;
+    cout << "Them 1 dong nua vao test1.cpp local" << endl;
     system("pause");
     return 0;
 }
