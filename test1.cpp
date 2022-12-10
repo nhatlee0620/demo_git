dadcc4fab4af3797ebc5a00b7f1f7a45e745facb
Lê Anh Nhật <91233904+nhatlee0620@users.noreply.github.com>
2022-12-10 12:29:43 +0700

    Update test1.cpp
    Update test1.cpp in repository

--- test1.cpp
+++ test1.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main() {
     cout << "Hello day la file test1.cpp" << endl;
     cout << "Them 1 dong trong test1.cpp" << endl;
+    cout << "Them 1 dong trong test1.cpp o tren repository" << endl;
     system("pause");
     return 0;
 }
