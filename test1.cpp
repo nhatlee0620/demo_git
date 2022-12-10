e642ddef31486e4f52f5c075dace09b890b54eab
nhatlee0620 <nhatlee0620@gmail.com>
2022-12-10 12:25:53 +0700

    fix test1.cpp and test2.cpp

--- test1.cpp
+++ test1.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main() {
     cout << "Hello day la file test1.cpp" << endl;
+    cout << "Them 1 dong trong test1.cpp" << endl;
     system("pause");
     return 0;
 }
--- test2.cpp
+++ test2.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main() {
     cout << "Hello day la file test2.cpp" << endl;
+    cout << "Them 1 dong trong test2.cpp" << endl;
     system("pause");
     return 0;
 }
