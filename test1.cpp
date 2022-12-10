4f097b7be1054ebce40cd8412570c6bd44002600
nhatlee0620 <nhatlee0620@gmail.com>
2022-12-10 12:33:59 +0700

    test conflict

--- test1.cpp
+++ test1.cpp
@@ -4,6 +4,7 @@ int main() {
     cout << "Hello day la file test1.cpp" << endl;
     cout << "Them 1 dong trong test1.cpp" << endl;
     cout << "Them 1 dong trong test1.cpp o tren repository" << endl;
+    cout << "Them 1 dong vao test1.cpp local" << endl;
     system("pause");
     return 0;
 }
