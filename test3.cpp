4aec4904a5123d9e5b70829db16b058551abd683
nhatlee0620 <nhatlee0620@gmail.com>
2023-01-30 23:25:56 +0700

    add test3.cpp 111

---
+++ test3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    vector<int> v;
+    for (int i = 0; i < 10; i++) {
+        v.push_back(i);
+    }
+    for (vector<int>::iterator it = v.begin(); it != v.end(); it++) {
+        cout << *it << " ";
+    }
+    cout << endl;
+    system("pause");
+    return 0;
+}
