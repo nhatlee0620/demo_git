212ab939e9d7430c00188e7d45e0eb4faccf826d
nhatlee0620 <nhatlee0620@gmail.com>
2023-02-14 22:12:29 +0700

    Push Folder 2

---
+++ Folder2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Folder2
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<string> lst = new List<string>();
+            lst.Add("one");
+            lst.Add("two");
+            lst.Add("three");
+            foreach(var str in lst)
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
