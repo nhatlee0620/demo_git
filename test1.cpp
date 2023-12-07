04fe5229d0a2cb8fcf15de73970f542e83f3cc7b
nhatlee0620 <nhatlee0620@gmail.com>
2023-12-07 22:02:08 +0700

    Update test1.cpp with function helloWorld()

--- test1.cpp
+++ test1.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 using namespace std;
+
+void helloWorld {
+    std::cout << "Hello World!" << std::endl;
+}
+
 int main() {
     cout << "Hello day la file test1.cpp" << endl;
     cout << "Them 1 dong trong test1.cpp" << endl;
